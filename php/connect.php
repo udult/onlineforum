630e73fb130d9f47139c199ad9097ab8405a2bcf
udult <152766044+udult@users.noreply.github.com>
2023-12-08 16:54:38 +0300

    connect.php

--- php/connect.php
+++ php/connect.php
@@ -2,8 +2,8 @@
 	$port = 5432;
 	$host = 'localhost';
 	$pg_user = 'postgres';
-	$data_base = 'onlineforum_lenina_packet';
-	$pg_password = 'some_password';
+	$data_base = '*********';
+	$pg_password = '********';
 	$hash_algo = 'sha256';
 
 	$conn = pg_connect("host=$host port=$port dbname=$data_base user=$pg_user password=$pg_password");
